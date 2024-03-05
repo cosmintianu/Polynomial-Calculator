618d641b2017801f69a6f6b20804becb43edc397
cosmintianu <cosmin.tianunicolae@yahoo.com>
2024-03-05 21:56:26 +0200

    Added gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import controller.CalculatorController;
+import view.CalculatorView;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
+        CalculatorController calculatorController = new CalculatorController();
+        CalculatorView calculatorView = new CalculatorView(calculatorController);
+
     }
-    }
+}
